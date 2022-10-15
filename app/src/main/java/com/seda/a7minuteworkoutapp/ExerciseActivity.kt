89e8e884af08e2d051a605ed84f3bf3dabaa6f45
Seda Nur Önder <sednrondr@gmail.com>
2022-10-15 23:43:05 +0300

    commit

---
+++ app/src/main/java/com/seda/a7minuteworkoutapp/ExerciseActivity.kt
@@ -0,0 +1,11 @@
+package com.seda.a7minuteworkoutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExerciseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise)
+    }
+}
--- app/src/main/java/com/seda/a7minuteworkoutapp/MainActivity.kt
+++ app/src/main/java/com/seda/a7minuteworkoutapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.seda.a7minuteworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.seda.a7minuteworkoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding : ActivityMainBinding ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding =ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding?.root)
+        binding?.flStart?.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Here we will start the exercise.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //Todo 4: TO avoid memory leak we unassign the binding once the activity is destroyed
+        binding = null
     }
 }
